31db9c27e22cf885a77082c4b02b914b48a42b05
Sean Dewar <6256228+seandewar@users.noreply.github.com>
2019-10-13 19:18:15 +0100

    Add complexity to remove duplicates from sorted array problem

--- easy/remove-duplicates-from-sorted-array.cpp
+++ easy/remove-duplicates-from-sorted-array.cpp
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 //
-// Solution below is more-or-less equivalent to:
+// Solution is more-or-less equivalent to:
 //
 // // vector::size_type is more correct, but function returns int
 // int newSize = 0;
@@ -11,18 +11,19 @@
 // }
 // return newSize;
 //
-// The solution can be changed to resize the resulting vector to the new size
-// as so:
+// Complexity: runtime O(n), space O(1).
+//
+// The solution can be changed to resize the resulting vector to the new size:
 //
 // nums.erase(unique(begin(nums), end(nums)), end(nums));
 // return size(nums);
 //
 // However, this is slower (unless the size is equivalent) as it's similar to
 // resize()ing the vector to the new, lesser size, which destructs all objects
-// outside the new range - O(n) complexity.  (This could technically be
-// completely optimized away (O(1)) if the objects are trivially destructable,
-// but the standard does not mandate this for resize() to smaller N or erase()
-// from M to N.
+// outside the new range - additional O(n) complexity. (This could technically
+// be O(1) optimized if the objects are trivially destructable, but the
+// standard doesn't mandate this for resize() to smaller N or erase() from M to
+// N.
 
 class Solution {
 public:
