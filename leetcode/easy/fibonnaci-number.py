f3fc29340216d7519b0ef290cf574152dfe25972
Sean Dewar <6256228+seandewar@users.noreply.github.com>
2019-10-30 12:59:24 +0000

    Add python 3 fibonnaci number solution

---
+++ leetcode/easy/fibonnaci-number.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/fibonacci-number/
+#
+# Uses Python 3.6. For Python 3.8 and above, you can replace reduce() by using
+# assignment expressions (:= operator).
+
+# Complexity: runtime O(n), space O(1).
+
+from functools import reduce
+
+class Solution:
+    def fib(self, N: int) -> int:
+        return reduce(lambda t, _ : (t[1], t[1] + t[0]), range(0, N), (0, 1))[0]
