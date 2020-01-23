577db11dd5a27cf5fcc692816a0b1332abfe3945
Sean Dewar <6256228+seandewar@users.noreply.github.com>
2020-01-23 04:27:41 +0000

    Symmetric Tree: use Java 10 var & final by default

--- leetcode/easy/symmetric-tree.java
+++ leetcode/easy/symmetric-tree.java
@@ -13,7 +13,7 @@
  * }
  */
 class Solution {
-    private boolean isSymmetricImpl(TreeNode left, TreeNode right) {
+    private boolean isSymmetricImpl(final TreeNode left, final TreeNode right) {
         if (left == null || right == null)
             return left == null && right == null;
 
@@ -22,7 +22,7 @@ class Solution {
                && isSymmetricImpl(left.right, right.left);
     }
 
-    public boolean isSymmetric(TreeNode root) {
+    public boolean isSymmetric(final TreeNode root) {
         if (root == null)
             return true;
 
@@ -34,28 +34,20 @@ class Solution {
 // Complexity: runtime O(n), space O(n) [if tree consists of a single chain of
 // length n].
 /*
-import java.util.Optional; // leetcode doesn't automatically import this
+import java.util.Optional;
 
 class Solution {
-    public boolean isSymmetric(TreeNode root) {
+    public boolean isSymmetric(final TreeNode root) {
         if (root == null)
             return true;
 
-        // java doesn't allow the inserting of null in some containers such as
-        // Deque (but sometimes does for Queue). this is because java is a meme
-        // language that uses null as a return value for some member functions
-        // to indicate an empty container (e.g Deque.poll()). we get around
-        // this by using Optional, although we could also use a Queue that
-        // allows null insertions (but java doesn't recommend inserting nulls
-        // even if the container allows it anyway...)
-        Deque<Optional<TreeNode>> stack = new ArrayDeque<>();
-
+        final var stack = new ArrayDeque<Optional<TreeNode>>();
         stack.add(Optional.ofNullable(root.left));
         stack.add(Optional.ofNullable(root.right));
 
         while (!stack.isEmpty()) {
-            Optional<TreeNode> right = stack.pop();
-            Optional<TreeNode> left = stack.pop();
+            final var right = stack.pop();
+            final var left = stack.pop();
 
             if (!left.isPresent() && !right.isPresent())
                 continue;
