d58a65962fc5809de23be08cfee17f3abc8be1d4
Sean Dewar <6256228+seandewar@users.noreply.github.com>
2020-02-16 17:24:37 +0000

    Add 3Sum O(n^2) runtime solution

--- leetcode/medium/3sum.cpp
+++ leetcode/medium/3sum.cpp
@@ -1,6 +1,15 @@
 // https://leetcode.com/problems/3sum/
 //
-// See code comments for details.
+// This solution sorts the input and uses 2 iterators to select 2 candidate
+// numbers for our sum, then uses a binary search to search for the 3rd number
+// needed to make the sum of our numbers == 0; if it succeeds, the triplet is
+// added to our output.
+//
+// This solution avoids outputting duplicate triplets by fast-forwarding each
+// iterator past any numbers that are duplicates of the number we just used as a
+// candidate. We can do this easily due to the input being sorted.
+//
+// See code comments for more details.
 //
 // Complexity: runtime O(n^2*logn), space O(1) [output not considered in
 // complexity calculations].
@@ -53,3 +62,68 @@ public:
         return results;
     }
 };
+
+// Alternative Solution: use a hash map to look-up the 3rd number, rather than
+// a binary search; faster runtime in exchange for more space.
+//
+// Complexity: runtime O(n^2), space O(n).
+/*
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) const
+    {
+        vector<vector<int>> results;
+
+        sort(begin(nums), end(nums));
+
+        // because nums is sorted, if there are duplicates of a number in nums,
+        // the last occurrence of the number will be the one whose index is
+        // saved in the map (earlier indices will be overwritten by the loop)
+        unordered_map<int, vector<int>::size_type> numsMap;
+
+        for (vector<int>::size_type i = 0; i < size(nums); ++i)
+            numsMap[nums[i]] = i;
+
+        for (auto it1 = cbegin(nums); it1 != cend(nums);) {
+            const int num1 = *it1;
+
+            if (num1 > 0)
+                break;
+
+            for (auto it2 = it1 + 1; it2 != cend(nums);) {
+                const int num2 = *it2;
+
+                if (num1 + num2 > 0)
+                    break;
+
+                const int needed = -(num1 + num2);
+
+                // use hash map to lookup the missing number's index, and
+                // ensure it exists after this iterator (after num2 in the
+                // sequence)
+                //
+                // because the map stores the index of the last occurrence of a
+                // number in nums (important if there are duplicates), we can
+                // safely test that the stored index > our current to ensure
+                // that at least 1 copy of the missing number exists in the
+                // last part of nums that we can use
+                //
+                // NOTE: the > comparison causes implicit conversion to
+                // unsigned, as rhs type (vector::difference_type) is signed
+                // (vs lhs size_type used in our map). this is fine, though
+                if (const auto fIt = numsMap.find(needed);
+                    fIt != cend(numsMap) && fIt->second > it2 - cbegin(nums))
+                    results.push_back({num1, num2, needed});
+
+                it2 = find_if(it2 + 1, cend(nums),
+                              [=](const int x) { return x != num2; });
+            }
+
+            it1 = find_if(it1 + 1, cend(nums),
+                          [=](const int x) { return x != num1; });
+        }
+
+        return results;
+    }
+};
+*/
