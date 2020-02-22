f7a21a7879827c1bfaab932dd770baa0d440ace8
Sean Dewar <6256228+seandewar@users.noreply.github.com>
2020-02-22 23:19:56 +0000

    Add Generate Parentheses sol

---
+++ leetcode/medium/generate-parentheses.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/generate-parentheses/
+//
+// Uses backtracking.
+// Complexity: complexity of generating n Catalan numbers: runtime
+// O(4^n/sqrt(n)), space O(n+(4^n/sqrt(n))) [from the leetcode analysis]
+
+class Solution {
+    void generate(vector<string>& out, string& combination,
+                  const int numRemaining, const int numUnclosed = 0) const
+    {
+        if (numRemaining < 0 || numUnclosed < 0)
+            return; // ill-formed parens in combination
+
+        if (numRemaining == 0 && numUnclosed == 0) {
+            out.push_back(combination);
+            return; // completed a valid combination
+        }
+
+        generate(out, combination += '(', numRemaining - 1, numUnclosed + 1);
+        combination.pop_back(); // backtrack the '('
+
+        generate(out, combination += ')', numRemaining, numUnclosed - 1);
+        combination.pop_back(); // backtrack the ')' for our caller
+    }
+
+public:
+    vector<string> generateParenthesis(const int n) const
+    {
+        vector<string> results;
+        string combination;
+
+        generate(results, combination, n);
+        return results;
+    }
+};
