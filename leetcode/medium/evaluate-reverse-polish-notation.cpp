8c2f3d8d03d4e75f49c58e84e1bd99c19646b104
Sean Dewar <6256228+seandewar@users.noreply.github.com>
2020-02-26 16:26:48 +0000

    Add alternative solution for Eval Reverse Polish

--- leetcode/medium/evaluate-reverse-polish-notation.cpp
+++ leetcode/medium/evaluate-reverse-polish-notation.cpp
@@ -1,5 +1,8 @@
 // https://leetcode.com/problems/evaluate-reverse-polish-notation/
 //
+// This is the first solution that came to mind. See my alternative solution for
+// an optimized version (same runtime/space complexity).
+//
 // Complexity: runtime O(n), space O(n).
 
 class Solution {
@@ -32,3 +35,43 @@ public:
         return !empty(s) ? s.top() : 0;
     }
 };
+
+// Alternative Solution: operation to function hash map + std::from_chars().
+// Same complexity, but slightly faster than the above.
+//
+// Complexity: runtime O(n), space O(n).
+/*
+class Solution {
+public:
+    int evalRPN(const vector<string>& tokens) const
+    {
+        static const unordered_map<string_view, function<int (int, int)>>
+            opMap = {
+                {"+", plus<int>()},
+                {"-", minus<int>()},
+                {"*", multiplies<int>()},
+                {"/", divides<int>()}
+            };
+
+        stack<int> s;
+
+        for (const string_view t : tokens) { // implicit conv to string_view
+            if (const auto fIt = opMap.find(t); fIt != cend(opMap)) { // op
+                const int b = s.top();
+                s.pop();
+
+                const int a = s.top();
+                s.pop();
+
+                s.push(fIt->second(a, b));
+            } else { // assume valid int
+                int val;
+                from_chars(t.data(), t.data() + size(t), val);
+                s.push(val);
+            }
+        }
+
+        return !empty(s) ? s.top() : 0;
+    }
+};
+*/
