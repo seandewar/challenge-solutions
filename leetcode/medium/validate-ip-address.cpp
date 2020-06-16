1d6adfdc30213915a19a2d67b02fc50278f3065c
Sean Dewar <seandewar@users.noreply.github.com>
2020-06-16 16:36:57 +0100

    Add Validate IP address solutions

---
+++ leetcode/medium/validate-ip-address.cpp
@@ -0,0 +1,75 @@
+// https://leetcode.com/problems/validate-ip-address/
+//
+// Fully-regex solution.
+//
+// You could split the regex up into the match required for one IPvX number,
+// constructing the full regex at runtime or statically instead, but it's easier
+// to use the magic of vim instead. :)
+//
+// Complexity: runtime Omega(n) [n == size(ip)].
+
+class Solution {
+public:
+    string validIPAddress(const string& ip) const
+    {
+        // avoids matching trailing 0s, and ensures numbers are between 0 & 255
+        const regex v4("((?:[1-9]?\\d)|(?:1\\d\\d)|(?:2[0-4]\\d)|(?:25[0-5]))\."
+                       "((?:[1-9]?\\d)|(?:1\\d\\d)|(?:2[0-4]\\d)|(?:25[0-5]))\."
+                       "((?:[1-9]?\\d)|(?:1\\d\\d)|(?:2[0-4]\\d)|(?:25[0-5]))\."
+                       "((?:[1-9]?\\d)|(?:1\\d\\d)|(?:2[0-4]\\d)|(?:25[0-5]))");
+
+        // expects 1 to 4 hexadecimal digits per number (trailing 0s allowed)
+        const regex v6("([\\da-fA-F]{1,4}):([\\da-fA-F]{1,4}):"
+                       "([\\da-fA-F]{1,4}):([\\da-fA-F]{1,4}):"
+                       "([\\da-fA-F]{1,4}):([\\da-fA-F]{1,4}):"
+                       "([\\da-fA-F]{1,4}):([\\da-fA-F]{1,4})");
+
+        // change the rest into a one-line nested ternary if you're chaotic :)
+        if (regex_match(ip, v4))
+            return "IPv4";
+        else if (regex_match(ip, v6))
+            return "IPv6";
+
+        return "Neither";
+    }
+};
+
+// Alternative Solution: Full-regex solution for IPv6, but partial for IPv4;
+// checks IPv4 numbers < 256 manually instead to simplify its regex a bit.
+//
+// Complexity: runtime Omega(n) [n == size(ip)].
+
+class Solution {
+public:
+    string validIPAddress(const string& ip) const
+    {
+        // avoids matching trailing 0s, and ensures numbers are between 0 & 299;
+        // we will need to check at runtime later that they are < 256
+        const regex v4("((?:[1-9]?\\d)|(?:[12]\\d\\d))\."
+                       "((?:[1-9]?\\d)|(?:[12]\\d\\d))\."
+                       "((?:[1-9]?\\d)|(?:[12]\\d\\d))\."
+                       "((?:[1-9]?\\d)|(?:[12]\\d\\d))");
+
+        // expects 1 to 4 hexadecimal digits per number (trailing 0s allowed)
+        const regex v6("([\\da-fA-F]{1,4}):([\\da-fA-F]{1,4}):"
+                       "([\\da-fA-F]{1,4}):([\\da-fA-F]{1,4}):"
+                       "([\\da-fA-F]{1,4}):([\\da-fA-F]{1,4}):"
+                       "([\\da-fA-F]{1,4}):([\\da-fA-F]{1,4})");
+
+        if (smatch match; regex_match(ip, match, v4)) {
+            // check that the IPv4 numbers are < 256.
+            // NOTE: 1st submatch is always the full match; skip with
+            // cbegin(match) + 1
+            if (all_of(cbegin(match) + 1, cend(match), [](const auto& subm) {
+                    return stoul(subm.str()) < 256;
+                })) {
+                return "IPv4";
+            }
+        }
+        else if (regex_match(ip, v6)) {
+            return "IPv6";
+        }
+
+        return "Neither";
+    }
+};
