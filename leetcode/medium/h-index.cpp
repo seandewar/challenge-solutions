77135e148f261098918fd162f3362fffde18ad8e
Sean Dewar <seandewar@users.noreply.github.com>
2020-08-12 22:11:53 +0100

    Add H-Index solution

---
+++ leetcode/medium/h-index.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/h-index/
+//
+// Complexity: runtime O(n*logn), space O(1).
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) const noexcept
+    {
+        sort(begin(citations), end(citations));
+
+        // a binary search can be done here instead, but this doesn't change the
+        // worst-case runtime complexity
+        for (size_t i = 0; i < size(citations); ++i) {
+            if (const auto h = size(citations) - i; h <= citations[i]) {
+                return h;
+            }
+        }
+
+        return 0;
+    }
+};
