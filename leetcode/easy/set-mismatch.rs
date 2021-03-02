02c530f0cef1ee0bef6d995cbd06f77f434c2dc7
Sean Dewar <seandewar@users.noreply.github.com>
2021-03-02 16:41:26 +0000

    Add Set Mismatch Solution

---
+++ leetcode/easy/set-mismatch.rs
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/set-mismatch
+//
+// HashSet to find duplicate num, XOR to find the missing num.
+// Complexity: runtime O(n), space O(n).
+
+impl Solution {
+    pub fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
+        let miss_xor_dup = nums
+            .iter()
+            .enumerate()
+            .fold(0, |acc, (i, &x)| acc ^ (i as i32 + 1) ^ x);
+
+        let mut set = std::collections::HashSet::new();
+        nums.into_iter()
+            .find(|x| !set.insert(x))
+            .map(|x| vec![x, miss_xor_dup ^ x])
+            .unwrap()
+    }
+}
+
+// Alternative Solution: Sorting + Iterator fun.
+// Complexity: runtime O(n*logn), space O(logn).
+
+impl Solution {
+    pub fn find_error_nums(mut nums: Vec<i32>) -> Vec<i32> {
+        nums.sort_unstable();
+
+        let last_dup_idx = (1..nums.len()).find(|&i| nums[i] == nums[i - 1]).unwrap();
+        let miss = (1..=last_dup_idx as i32)
+            .chain(last_dup_idx as i32..nums.len() as _)
+            .zip(nums.iter())
+            .find(|&(expect, &x)| expect != x)
+            .map(|(miss, _)| miss)
+            .unwrap_or(nums.len() as _);
+
+        vec![nums[last_dup_idx], miss]
+    }
+}
