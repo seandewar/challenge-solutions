4519231ac79427f748364d3619246fb395aa46c6
Sean Dewar <seandewar@users.noreply.github.com>
2021-03-02 20:23:02 +0000

    Add Set Mismatch Single-pass O(n) runtime, O(1) space solution

--- leetcode/easy/set-mismatch.rs
+++ leetcode/easy/set-mismatch.rs
@@ -18,6 +18,27 @@ impl Solution {
     }
 }
 
+// Alternative Solution: XOR + Negate nums[x - 1] for each x in nums to isolate the duplicate.
+//
+// By doing this, the duplicate will be positive after the traversal.
+// We can negate as the values in nums are > 0.
+//
+// Complexity: runtime O(n), space O(1).
+
+impl Solution {
+    pub fn find_error_nums(mut nums: Vec<i32>) -> Vec<i32> {
+        let (miss_xor_dup, dup) = (0..nums.len()).fold((0, None), |(xor, dup), i| {
+            let (x, y) = (nums[i].abs(), i as i32 + 1);
+            let mark_i = x as usize - 1;
+            nums[mark_i] *= -1;
+
+            (xor ^ y ^ x, dup.or(Some(x).filter(|_| nums[mark_i] > 0)))
+        });
+
+        vec![dup.unwrap(), miss_xor_dup ^ dup.unwrap()]
+    }
+}
+
 // Alternative Solution: Sorting + Iterator fun.
 // Complexity: runtime O(n*logn), space O(logn).
 
