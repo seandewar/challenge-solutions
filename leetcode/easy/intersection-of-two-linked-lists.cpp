9d6df3c3cb72c5de1c603ae5c7d70aa4dd6b79aa
Sean Dewar <seandewar@users.noreply.github.com>
2021-03-04 21:21:01 +0000

    Add More Intersection Of Two Linked Lists Solutions

--- leetcode/easy/intersection-of-two-linked-lists.cpp
+++ leetcode/easy/intersection-of-two-linked-lists.cpp
@@ -77,9 +77,48 @@ public:
     }
 };
 
+// Alternative Solution: No mutation, constant space.
+//
+// In a list with an intersection point, there will be a number of nodes
+// (possibly 0) before the intersection point for A & B. Therefore:
+//
+// From headA: nodesA + intersection + nodesAB
+// From headB: nodesB + intersection + nodesAB
+//
+// If we traverse the lists from headA & headB, and restart the traversal from
+// the opposite head once we reach the end, if there is an intersection, we will
+// reach the intersection point at the same time after the restart! If there is
+// no intersection, we will reach the end of the list at the same time.
+//
+// The reason for this is that regardless of the distance of nodesA or nodesB,
+// both traversals will eventually step the same number of nodes once the
+// restart takes place at the other head:
+//
+// From headA: nodesA + intersection + nodesAB + nodesB + intersection
+// From headB: nodesB + intersection + nodesAB + nodesA + intersection
+//
+// Both nodes will travel: nodesA + nodesB + intersection + nodesAB nodes!
+//
+// Complexity: runtime O(n+m), space O(1).
+
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* const headA,
+                                  ListNode* const headB) const noexcept
+    {
+        auto nA = headA;
+        for (auto nB = headB; nA != nB;) {
+            nA = nA ? nA->next : headB;
+            nB = nB ? nB->next : headA;
+        }
+
+        return nA;
+    }
+};
+
 // Using a hash set.
 // Complexity: runtime O(n+m), space O(n+m).
-/*
+
 class Solution {
 public:
     ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) const
@@ -107,4 +146,48 @@ public:
         return nullptr;
     }
 };
-*/
+
+// Alternative Solution: Mutate the list to mark visits, then restore it after.
+// Complexity: runtime O(n+m), space O(1).
+
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* const headA,
+                                  ListNode* const headB) const noexcept
+    {
+        ListNode* res = nullptr;
+        for (auto nA = headA, nB = headB; nA || nB;) {
+            if (nA) {
+                if ((nA->val *= -1) > 0) {
+                    res = nA;
+                    break;
+                }
+                nA = nA->next;
+            }
+
+            if (nB) {
+                if ((nB->val *= -1) > 0) {
+                    res = nB;
+                    break;
+                }
+                nB = nB->next;
+            }
+        }
+
+        for (auto nA = headA, nB = headB; nA || nB != res;) {
+            if (nA) {
+                if (nA->val < 0)
+                    nA->val *= -1;
+
+                nA = nA->next;
+            }
+
+            if (nB != res) {
+                nB->val *= -1;
+                nB = nB->next;
+            }
+        }
+
+        return res;
+    }
+};
