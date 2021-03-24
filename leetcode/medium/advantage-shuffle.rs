265cb1d31c21ca4f8cf38fa5c72892e9db5aeda2
Sean Dewar <seandewar@users.noreply.github.com>
2021-03-24 12:29:35 +0000

    Add Binary Tree Solution for Advantage Shuffle

--- leetcode/medium/advantage-shuffle.rs
+++ leetcode/medium/advantage-shuffle.rs
@@ -1,7 +1,7 @@
 // https://leetcode.com/problems/advantage-shuffle
 //
 // Complexity: runtime O(n^2) worst-case due to remove() on each iteration, but runtime is likely
-//             to be closer to O(n*logn),
+//             to be closer to O(n*logn) [think of it as O(n*r + n*logn)],
 //             space O(n) [O(logn) ignoring output (sort_unstable())].
 
 impl Solution {
@@ -13,6 +13,30 @@ impl Solution {
                 a.remove(if i < a.len() { i } else { 0 })
             })
             .collect()
+    }
+}
+
+// Alternative Solution: Binary tree.
+// Complexity: runtime O(n*logn), space O(n).
+
+impl Solution {
+    pub fn advantage_count(mut a: Vec<i32>, b: Vec<i32>) -> Vec<i32> {
+        let mut rem = std::collections::BTreeMap::new();
+        a.iter().for_each(|&v| *rem.entry(v).or_insert(0) += 1);
+
+        for (x, y) in a.iter_mut().zip(b.into_iter()) {
+            let (&v, n) = match rem.range_mut(y + 1..).next() {
+                Some(entry) => entry,
+                None => rem.iter_mut().next().unwrap(),
+            };
+            *x = v;
+            *n -= 1;
+            if *n == 0 {
+                rem.remove(&v);
+            }
+        }
+
+        a
     }
 }
 
