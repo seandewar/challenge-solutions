e2f20fe4a6aabfa1acbc1710efee16fe8125fb20
Sean Dewar <seandewar@users.noreply.github.com>
2021-04-18 13:23:50 +0100

    Update Remove Nth Node From End of List Solution

--- leetcode/medium/remove-nth-node-from-end-of-list.cpp
+++ leetcode/medium/remove-nth-node-from-end-of-list.cpp
@@ -4,44 +4,26 @@
 // from the list end. Functionally equivalent to 1 pointer with 2 passes,
 // albeit this is slightly better on cache locality.
 //
+// "fast" has a n step head start in front of our slow pointer so that our slow
+// pointer refers to the node to delete after "fast" hits the end of the list
+// (our slow pointer starts at a node before the original head, so that we can
+// change the node before the node to delete to refer to the node after the
+// deleted node as the next node).
+//
+// "head" is re-used as the "slow" pointer.
+//
 // Complexity: runtime O(n), space O(1).
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n)
     {
-        // advance the fast ptr n steps ahead of slow
-        auto fast{head};
-        while (n-- > 0)
+        ListNode beforeHead(0, head), *fast = head;
+        for (; n > 0; --n)
             fast = fast->next;
-
-        // if n corresponds to the list head, fast will be null (fast is 1
-        // place past the list's end: n == list size). delete the head
-        if (!fast) {
-            const auto newHead = head->next;
-            delete head;
-            return newHead;
-        }
-
-        // advance both ptrs until fast reaches the end of the list
-        auto slow{head};
-        while (fast->next) {
-            fast = fast->next;
-            slow = slow->next;
-        }
-
-        // slow ptr points to the node before the one to be deleted
-        const auto nodeToDelete = slow->next;
-        slow->next = nodeToDelete->next;
-        delete nodeToDelete;
-        return head;
+        for (head = &beforeHead; fast; fast = fast->next)
+            head = head->next;
+        delete exchange(head->next, head->next->next);
+        return beforeHead.next;
     }
 };
