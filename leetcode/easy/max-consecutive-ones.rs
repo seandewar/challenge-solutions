2e4b91a6c7eafd83122013057075fced013de0b4
Sean Dewar <seandewar@users.noreply.github.com>
2021-04-29 23:06:48 +0100

    Add Max Consecutive Ones Solution

---
+++ leetcode/easy/max-consecutive-ones.rs
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/max-consecutive-ones
+//
+// Complexity: runtime O(n), space O(1).
+
+impl Solution {
+    pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
+        nums.iter()
+            .scan(0, |streak, &x| {
+                *streak += 1;
+                if x != 1 {
+                    *streak = 0;
+                }
+                Some(*streak)
+            })
+            .max()
+            .unwrap()
+    }
+}
