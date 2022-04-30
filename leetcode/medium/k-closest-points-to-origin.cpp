14557a079fb732dabac41999a87fd6ee1e282e0f
Sean Dewar <seandewar@users.noreply.github.com>
2022-04-30 14:55:49 +0100

    Update K Closest Points To Origin Solution

--- leetcode/medium/k-closest-points-to-origin.cpp
+++ leetcode/medium/k-closest-points-to-origin.cpp
@@ -1,19 +1,17 @@
 // https://leetcode.com/problems/k-closest-points-to-origin
-//
-// Complexity: runtime O(TODO), space O(TODO).
+// Complexity: runtime O(nlogk), space O(logn) ignoring output, O(k) otherwise.
 
 class Solution {
 public:
-    vector<vector<int>> kClosest(vector<vector<int>> &points, int K)
+    vector<vector<int>> kClosest(const vector<vector<int>> &points, const int K)
     {
-        auto res = points;
-        partial_sort(begin(res), end(res), begin(res),
-                     [](const auto &a, const auto &b) noexcept {
-                         const auto da = a[0] * a[0] + a[1] * a[1];
-                         const auto db = b[0] * b[0] + b[1] * b[1];
-                         return da < db;
-                     });
-        res.erase(begin(res) + K, end(res));
+        vector<vector<int>> res(K);
+        partial_sort_copy(cbegin(points), cend(points), begin(res), end(res),
+                          [](const auto &a, const auto &b) noexcept {
+                              const auto da = a[0] * a[0] + a[1] * a[1];
+                              const auto db = b[0] * b[0] + b[1] * b[1];
+                              return da < db;
+                          });
         return res;
     }
 };
