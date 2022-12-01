ad2b8e88297c216fcc15739026460b2838467649
Sean Dewar <seandewar@users.noreply.github.com>
2022-12-01 09:11:14 +0000

    Optimize AoC 22 D1

--- advent-of-code/2022/day1/src/main.zig
+++ advent-of-code/2022/day1/src/main.zig
@@ -1,37 +1,23 @@
 const std = @import("std");
 
-const elf_calories = blk: {
-    @setEvalBranchQuota(500_000);
+const top3 = blk: {
+    @setEvalBranchQuota(100_000);
     const input = @embedFile("input");
-    var result = [_]u32{0} ** (1 + std.mem.count(u8, input, "\n\n"));
+    var result = [_]u32{0} ** 3;
     var line_it = std.mem.split(u8, input, "\n");
-    var i: usize = 0;
+    var accum: u32 = 0;
     while (line_it.next()) |line| {
         if (line.len == 0) {
-            i += 1;
-            continue;
-        }
-        result[i] += std.fmt.parseInt(u32, line, 10) catch unreachable;
+            const i = std.mem.indexOfMin(u32, &result);
+            result[i] = @max(result[i], accum);
+            accum = 0;
+        } else accum += std.fmt.parseInt(u32, line, 10) catch unreachable;
     }
-
     break :blk result;
 };
 
-const top_elves = blk: {
-    @setEvalBranchQuota(10_000);
-    var result: [elf_calories.len]usize = std.simd.iota(usize, elf_calories.len);
-    std.sort.sort(usize, &result, {}, struct {
-        fn compare(context: void, i: usize, j: usize) bool {
-            _ = context;
-            return elf_calories[i] > elf_calories[j];
-        }
-    }.compare);
-
-    break :blk result;
-};
-
-const p1 = elf_calories[top_elves[0]];
-const p2 = p1 + elf_calories[top_elves[1]] + elf_calories[top_elves[2]];
+const p1 = std.mem.max(u32, &top3);
+const p2 = top3[0] + top3[1] + top3[2];
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
