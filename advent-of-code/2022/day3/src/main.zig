d7d9be2f82ba2b4a7ad2f751a8f3564757feaceb
Sean Dewar <seandewar@users.noreply.github.com>
2022-12-03 07:23:14 +0000

    Optimize AoC 2022 D3

--- advent-of-code/2022/day3/src/main.zig
+++ advent-of-code/2022/day3/src/main.zig
@@ -1,45 +1,36 @@
 const std = @import("std");
 
-const input = @embedFile("input");
-
-const p1 = blk: {
+const answers = blk: {
     @setEvalBranchQuota(100_000);
+    const input = @embedFile("input");
     var line_it = std.mem.tokenize(u8, input, "\n");
-    var result: u32 = 0;
+    var p2_seen = [_]u3{0} ** (1 + 'z' - 'A');
+    var p2_elf_i = 0;
+    var p1_answer: u32 = 0;
+    var p2_answer: u32 = 0;
     while (line_it.next()) |line| {
-        var seen = [_]bool{false} ** (1 + 'z' - 'A');
-        for (line[0 .. line.len / 2]) |item| seen[item - 'A'] = true;
+        var p1_seen = [_]bool{false} ** (1 + 'z' - 'A');
+        for (line[0 .. line.len / 2]) |item| p1_seen[item - 'A'] = true;
         for (line[line.len / 2 ..]) |item| {
-            if (seen[item - 'A']) {
-                result += if (std.ascii.isLower(item)) item - 'a' + 1 else item - 'A' + 27;
+            if (p1_seen[item - 'A']) {
+                p1_answer += if (std.ascii.isLower(item)) item - 'a' + 1 else item - 'A' + 27;
                 break;
             }
         }
-    }
-    break :blk result;
-};
-
-const p2 = blk: {
-    @setEvalBranchQuota(100_000);
-    var line_it = std.mem.tokenize(u8, input, "\n");
-    var seen = [_]u3{0} ** (1 + 'z' - 'A');
-    var elf_i = 0;
-    var result: u32 = 0;
-    while (line_it.next()) |line| {
         for (line) |item| {
-            seen[item - 'A'] |= 1 << elf_i;
-            if (seen[item - 'A'] == 0b111) {
-                result += if (std.ascii.isLower(item)) item - 'a' + 1 else item - 'A' + 27;
-                std.mem.set(u3, &seen, 0);
+            p2_seen[item - 'A'] |= 1 << p2_elf_i;
+            if (p2_seen[item - 'A'] == 0b111) {
+                p2_answer += if (std.ascii.isLower(item)) item - 'a' + 1 else item - 'A' + 27;
+                std.mem.set(u3, &p2_seen, 0);
                 break;
             }
         }
-        elf_i = (elf_i + 1) % 3;
+        p2_elf_i = (p2_elf_i + 1) % 3;
     }
-    break :blk result;
+    break :blk .{ p1_answer, p2_answer };
 };
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("Day3 (comptime only): P1: {}, P2: {}\n", .{ p1, p2 });
+    try stdout.print("Day3 (comptime only): P1: {}, P2: {}\n", answers);
 }
