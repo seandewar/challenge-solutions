a1559ab10f766a1188d954a9bf9a0bdafc5e8359
Sean Dewar <seandewar@users.noreply.github.com>
2022-12-07 11:06:53 +0000

    AoC 2022 D7: Use pointers over indices
    It's a bit nicer (our indices were already usize anyway)

--- advent-of-code/2022/day7/src/main.zig
+++ advent-of-code/2022/day7/src/main.zig
@@ -2,37 +2,38 @@ const std = @import("std");
 
 const node_buf = blk: {
     const Node = struct {
+        const Children = std.BoundedArray(*@This(), 64);
         name: []const u8,
         size: u32 = 0,
-        parent_i: ?usize,
-        child_indices: std.BoundedArray(usize, 64) = std.BoundedArray(usize, 64).init(0) catch unreachable,
+        parent: ?*@This(),
+        children: Children = Children.init(0) catch unreachable,
     };
     @setEvalBranchQuota(1_000_000);
     const input = @embedFile("input");
     var buf = std.BoundedArray(Node, 1024).init(0) catch unreachable;
-    buf.append(.{ .name = "", .parent_i = null }) catch unreachable;
-    var cwd_i: usize = 0;
+    buf.append(.{ .name = "", .parent = null }) catch unreachable;
+    var cwd = &buf.slice()[0];
     var line_it = std.mem.tokenize(u8, input, "\n");
     _ = line_it.next().?; // Skip "$ cd /" (we already start in the root directory).
     while (line_it.next()) |line| {
         if (std.mem.startsWith(u8, line, "dir ")) { // ls child directory
-            buf.append(.{ .name = line["dir ".len..], .parent_i = cwd_i }) catch unreachable;
-            buf.slice()[cwd_i].child_indices.append(buf.len - 1) catch unreachable;
+            buf.append(.{ .name = line["dir ".len..], .parent = cwd }) catch unreachable;
+            cwd.children.append(&buf.slice()[buf.len - 1]) catch unreachable;
         } else if (std.ascii.isDigit(line[0])) { // ls child file
             const slice = line[0..std.mem.indexOfScalar(u8, line, ' ').?];
             const size = std.fmt.parseInt(u32, slice, 10) catch unreachable;
-            var i = cwd_i;
-            while (true) {
-                buf.slice()[i].size += size;
-                if (buf.get(i).parent_i) |parent_i| i = parent_i else break;
+            var dir_node: ?*Node = cwd;
+            while (dir_node) |dir| {
+                dir.size += size;
+                dir_node = dir.parent;
             }
         } else if (std.mem.startsWith(u8, line, "$ cd ")) { // change directory
             const dir = line["$ cd ".len..];
             if (std.mem.eql(u8, dir, "..")) {
-                cwd_i = buf.get(cwd_i).parent_i.?;
-            } else for (buf.get(cwd_i).child_indices.slice()) |i| {
-                if (std.mem.eql(u8, buf.get(i).name, dir)) {
-                    cwd_i = i;
+                cwd = cwd.parent.?;
+            } else for (cwd.children.slice()) |child| {
+                if (std.mem.eql(u8, child.name, dir)) {
+                    cwd = child;
                     break;
                 }
             }
