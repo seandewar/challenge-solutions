5f9dde6c410fcaacbaff4ac18ce409b674d9fd88
Sean Dewar <seandewar@users.noreply.github.com>
2022-12-08 18:33:53 +0000

    AoC 2022: simplify

--- advent-of-code/2022/day3/src/main.zig
+++ advent-of-code/2022/day3/src/main.zig
@@ -7,28 +7,28 @@ const answers = blk: {
     const ElfSet = std.StaticBitSet(3);
     var p2_seen = [_]ElfSet{ElfSet.initEmpty()} ** (1 + 'z' - 'A');
     var p2_elf_i = 0;
-    var p1_answer: u32 = 0;
-    var p2_answer: u32 = 0;
+    var p1 = 0;
+    var p2 = 0;
     while (line_it.next()) |line| {
         var p1_seen = [_]bool{false} ** (1 + 'z' - 'A');
         for (line[0 .. line.len / 2]) |item| p1_seen[item - 'A'] = true;
         for (line[line.len / 2 ..]) |item| {
             if (p1_seen[item - 'A']) {
-                p1_answer += if (std.ascii.isLower(item)) item - 'a' + 1 else item - 'A' + 27;
+                p1 += if (std.ascii.isLower(item)) item - 'a' + 1 else item - 'A' + 27;
                 break;
             }
         }
         for (line) |item| {
             p2_seen[item - 'A'].set(p2_elf_i);
             if (p2_seen[item - 'A'].count() == 3) {
-                p2_answer += if (std.ascii.isLower(item)) item - 'a' + 1 else item - 'A' + 27;
+                p2 += if (std.ascii.isLower(item)) item - 'a' + 1 else item - 'A' + 27;
                 for (p2_seen) |*set| set.mask = 0;
                 break;
             }
         }
         p2_elf_i = (p2_elf_i + 1) % 3;
     }
-    break :blk .{ p1_answer, p2_answer };
+    break :blk .{ p1, p2 };
 };
 
 pub fn main() !void {
