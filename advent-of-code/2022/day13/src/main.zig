9fb40c811c96c62adaaa829efe751d4c1fd75466
Sean Dewar <seandewar@users.noreply.github.com>
2022-12-13 16:49:43 +0000

    AoC 2022 D13: Simplify
    No need to actually sort, just count the number of packets less than the
    decoders.

--- advent-of-code/2022/day13/src/main.zig
+++ advent-of-code/2022/day13/src/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
-fn isOrdered(lines: [2][]const u8) ?bool {
-    var pair = lines;
+fn lessThan(lhs: []const u8, rhs: []const u8) ?bool {
+    var pair = [_][]const u8{ lhs, rhs };
     while (pair[0].len != 0 and pair[1].len != 0) {
         if (pair[0][0] == '[' or pair[1][0] == '[') { // List vs List or List vs Integer.
             var inner_pair = pair;
@@ -24,7 +24,7 @@ fn isOrdered(lines: [2][]const u8) ?bool {
                 packet.* = packet.*[1 .. 1 + len];
                 pair[pair_i] = pair[pair_i][2 + len ..];
             }
-            if (isOrdered(inner_pair)) |ordered| return ordered;
+            if (lessThan(inner_pair[0], inner_pair[1])) |lt| return lt;
         } else { // Integer vs Integer.
             var ints: [2]u8 = undefined;
             for (pair) |*packet, pair_i| {
@@ -39,36 +39,23 @@ fn isOrdered(lines: [2][]const u8) ?bool {
             if (packet.len != 0 and packet.*[0] == ',') packet.* = packet.*[1..];
         }
     }
-    if (pair[0].len == 0 and pair[1].len == 0) return null; // Inconclusive.
+    if (pair[0].len == 0 and pair[1].len == 0) return null; // Equal.
     return pair[0].len == 0;
-}
-
-fn lessThan(context: void, lhs: []const u8, rhs: []const u8) bool {
-    _ = context;
-    return isOrdered(.{ lhs[1 .. lhs.len - 1], rhs[1 .. rhs.len - 1] }).?;
 }
 const answers = blk: {
     @setEvalBranchQuota(1_000_000);
     const input = @embedFile("input");
-    var packet_count = 2; // Make room for decoder packets.
-    var line_it = std.mem.tokenize(u8, input, std.cstr.line_sep);
-    while (line_it.next()) |_| packet_count += 1;
-    var packets = std.BoundedArray([]const u8, packet_count).init(0) catch unreachable;
-    packets.appendSlice(&.{ "[[2]]", "[[6]]" }) catch unreachable;
     var p1 = 0;
+    var p2_decoder_i: [2]usize = if (lessThan("[[2]]", "[[6]]").?) .{ 1, 2 } else .{ 2, 1 };
     var pair_i = 1;
-    line_it = std.mem.tokenize(u8, input, std.cstr.line_sep);
+    var line_it = std.mem.tokenize(u8, input, std.cstr.line_sep);
     while (line_it.next()) |lhs| : (pair_i += 1) {
         const rhs = line_it.next().?;
-        packets.appendSlice(&.{ lhs, rhs }) catch unreachable;
-        if (lessThan({}, lhs, rhs)) p1 += pair_i;
+        if (lessThan(lhs, rhs).?) p1 += pair_i;
+        p2_decoder_i[0] += @boolToInt(lessThan(lhs, "[[2]]").?) + @boolToInt(lessThan(rhs, "[[2]]").?);
+        p2_decoder_i[1] += @boolToInt(lessThan(lhs, "[[6]]").?) + @boolToInt(lessThan(rhs, "[[6]]").?);
     }
-    std.sort.sort([]const u8, packets.slice(), {}, lessThan);
-    var p2 = 1;
-    for (packets.slice()) |packet, i| {
-        if (std.mem.eql(u8, packet, "[[2]]") or std.mem.eql(u8, packet, "[[6]]")) p2 *= i + 1;
-    }
-    break :blk .{ p1, p2 };
+    break :blk .{ p1, p2_decoder_i[0] * p2_decoder_i[1] };
 };
 
 pub fn main() !void {
