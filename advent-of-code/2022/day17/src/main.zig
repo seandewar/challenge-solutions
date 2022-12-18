6008149bc1673b7411324e897a89f599d94f2cd5
Sean Dewar <seandewar@users.noreply.github.com>
2022-12-18 21:58:28 +0000

    AoC 2022: D17 P1 optimization
    We only really need to keep track of the grid from the last point where a
    7-block-wide floor was formed. Do this to save memory.

--- advent-of-code/2022/day17/src/main.zig
+++ advent-of-code/2022/day17/src/main.zig
@@ -2,8 +2,6 @@
 //! happen when the blocks create a 7 tile-wide floor, and the next block in
 //! the sequence starts falling at the same position in the command cycle
 //! that we were at when there was previously a 7 tile-wide floor!
-//! We can also simply reset the grid when a 7 tile-wide floor is created, as the blocks
-//! below will not affect the positions of any future blocks.
 const std = @import("std");
 
 const shapes = [_]u16{
@@ -20,13 +18,13 @@ const cmds = blk: { // Just consider the first line.
     break :blk input[0 .. std.mem.indexOf(u8, input, std.cstr.line_sep) orelse input.len];
 };
 
-fn shapeDo(grid: [][7]bool, shape_i: usize, shape_x: u8, shape_y: usize, comptime action: enum {
+fn shapeDo(grid: [][7]bool, floor_y: usize, shape_i: usize, shape_x: u8, shape_y: usize, comptime action: enum {
     validateX,
     validateY,
     rest,
-}) union(enum) { is_valid: bool, min_y: usize } {
+}) union(enum) { is_valid: bool, max_y: usize } {
     var i: u8 = 0;
-    var min_y: usize = std.math.maxInt(usize);
+    var max_y: usize = 0;
     var shape = shapes[shape_i % shapes.len];
     while (shape != 0) : ({
         i += 1;
@@ -34,44 +32,65 @@ fn shapeDo(grid: [][7]bool, shape_i: usize, shape_x: u8, shape_y: usize, comptim
     }) {
         if (shape & 1 == 0) continue;
         const x = shape_x + 3 - (i % 4);
-        const y = shape_y + 3 - (i / 4);
-        min_y = @min(min_y, y);
+        const y = shape_y + i / 4;
+        max_y = @max(max_y, y);
         switch (action) {
-            .validateX => if (x >= 7 or grid[y][x]) break,
-            .validateY => if (y >= grid.len or grid[y][x]) break,
-            .rest => grid[y][x] = true,
+            .validateX => if (x >= 7 or grid[y - 1 - floor_y][x]) break,
+            .validateY => if (y <= floor_y or grid[y - 1 - floor_y][x]) break,
+            .rest => grid[y - 1 - floor_y][x] = true,
         }
     }
     return switch (action) {
         .validateX, .validateY => .{ .is_valid = shape == 0 },
-        .rest => .{ .min_y = min_y },
+        .rest => .{ .max_y = max_y },
     };
 }
 pub fn main() !void {
-    var grid = [_][7]bool{.{false} ** 7} ** p1_max_height;
-    var min_y = grid.len;
+    var grid = [_][7]bool{.{false} ** 7} ** p1_max_height; // Index 0 is the bottom-most point of the grid.
+    var floor_y: usize = 0;
+    var max_y: usize = 0;
     var shape_i: usize = 0;
-    var shape_x: u8 = 2;
-    var shape_y: usize = grid.len - 7;
+    var shape_x: u8 = 2; // Bottom-left of the shape.
+    var shape_y: usize = 4;
     var cmd_i: usize = 0;
-    while (shape_i < 2022) {
+    var p1: usize = 0;
+    while (true) {
         const new_shape_x = if (cmds[cmd_i] == '<') shape_x -| 1 else shape_x + 1;
         cmd_i = (cmd_i + 1) % cmds.len;
-        if (new_shape_x != shape_x and shapeDo(&grid, shape_i, new_shape_x, shape_y, .validateX).is_valid)
+        if (new_shape_x != shape_x and shapeDo(&grid, floor_y, shape_i, new_shape_x, shape_y, .validateX).is_valid)
             shape_x = new_shape_x;
-        if (!shapeDo(&grid, shape_i, shape_x, shape_y + 1, .validateY).is_valid) {
-            min_y = @min(min_y, shapeDo(&grid, shape_i, shape_x, shape_y, .rest).min_y);
+        if (!shapeDo(&grid, floor_y, shape_i, shape_x, shape_y - 1, .validateY).is_valid) {
+            max_y = @max(max_y, shapeDo(&grid, floor_y, shape_i, shape_x, shape_y, .rest).max_y);
+            // Check for any new 7-block floors formed by this piece. If so, we
+            // can start the grid from after the new floor_y to save memory.
+            var rev_it = std.mem.reverseIterator(grid[shape_y - 1 - floor_y .. shape_y - 1 - floor_y + 4]);
+            while (rev_it.next()) |row| {
+                if (!std.mem.allEqual(bool, &row, true)) continue;
+                const new_floor_y = shape_y + rev_it.index;
+                std.mem.copy([7]bool, &grid, grid[new_floor_y - floor_y .. max_y - floor_y]);
+                std.mem.set([7]bool, grid[max_y - new_floor_y .. max_y - floor_y], .{false} ** 7);
+                floor_y = new_floor_y;
+                break;
+            }
             shape_i += 1;
             shape_x = 2;
-            shape_y = min_y - 7;
-        } else shape_y += 1;
+            shape_y = max_y + 4;
+            if (shape_i == 2022) {
+                p1 = max_y;
+                break; // TODO: Part 2
+            }
+        } else shape_y -= 1;
     }
-    for (grid[min_y..]) |row| {
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Day17 (comptime parsing): P1: {}, P2: {}\n", .{ p1, 0 });
+}
+
+fn debugPrintGrid(grid: []const [7]bool, floor_y: usize, max_y: usize) void {
+    var rev_it = std.mem.reverseIterator(grid[0 .. max_y - floor_y]);
+    while (rev_it.next()) |row| {
         std.debug.print("|", .{});
-        for (row) |cell| std.debug.print("{s}", .{if (cell) "#" else "."});
+        for (row) |solid| std.debug.print("{s}", .{if (solid) "#" else "."});
         std.debug.print("|\n", .{});
     }
-    std.debug.print("---------\n", .{});
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("Day17 (comptime parsing): P1: {}\n", .{p1_max_height - min_y});
+    std.debug.print("+-------+ <- y={}\n", .{floor_y});
 }
