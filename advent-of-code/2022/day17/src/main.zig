2fb2e3bebccec8a900042bdffa66751bb9d857a6
Sean Dewar <seandewar@users.noreply.github.com>
2022-12-18 23:16:47 +0000

    AoC 2022: D17
    This may not work for all inputs depending on whether or not a 7-tile-wide floor
    is made.

--- advent-of-code/2022/day17/src/main.zig
+++ advent-of-code/2022/day17/src/main.zig
@@ -1,9 +1,4 @@
-//! TODO: for part 2, at some point, I think the grid will repeat: this will
-//! happen when the blocks create a 7 tile-wide floor, and the next block in
-//! the sequence starts falling at the same position in the command cycle
-//! that we were at when there was previously a 7 tile-wide floor!
 const std = @import("std");
-
 const shapes = [_]u16{
     0b0000_0000_0000_1111,
     0b0000_0100_1110_0100,
@@ -11,7 +6,6 @@ const shapes = [_]u16{
     0b1000_1000_1000_1000,
     0b0000_0000_1100_1100,
 };
-const p1_max_height = 3 + 2022 * 4;
 const cmds = blk: { // Just consider the first line.
     @setEvalBranchQuota(100_000);
     const input = @embedFile("input");
@@ -46,23 +40,24 @@ fn shapeDo(grid: [][7]bool, floor_y: usize, shape_i: usize, shape_x: u8, shape_y
     };
 }
 pub fn main() !void {
-    var grid = [_][7]bool{.{false} ** 7} ** p1_max_height; // Index 0 is the bottom-most point of the grid.
+    const stdout = std.io.getStdOut().writer();
+    var grid = [_][7]bool{.{false} ** 7} ** 8192; // Index 0 is the bottom-most point of the grid.
     var floor_y: usize = 0;
     var max_y: usize = 0;
     var shape_i: usize = 0;
     var shape_x: u8 = 2; // Bottom-left of the shape.
     var shape_y: usize = 4;
     var cmd_i: usize = 0;
-    var p1: usize = 0;
-    while (true) {
+    var repeat_start: ?struct { cmd_i: usize, shape_i: usize, max_y: usize } = null;
+    while (shape_i < 1_000_000_000_000) {
         const new_shape_x = if (cmds[cmd_i] == '<') shape_x -| 1 else shape_x + 1;
         cmd_i = (cmd_i + 1) % cmds.len;
         if (new_shape_x != shape_x and shapeDo(&grid, floor_y, shape_i, new_shape_x, shape_y, .validateX).is_valid)
             shape_x = new_shape_x;
         if (!shapeDo(&grid, floor_y, shape_i, shape_x, shape_y - 1, .validateY).is_valid) {
             max_y = @max(max_y, shapeDo(&grid, floor_y, shape_i, shape_x, shape_y, .rest).max_y);
-            // Check for any new 7-block floors formed by this piece. If so, we
-            // can start the grid from after the new floor_y to save memory.
+            // If a new 7-block-wide floor is formed by this piece, blocks under the floor are inaccessible and can be forgotten.
+            // The pattern may also be repeating if we're processing the same part of the input as a previous time a floor was formed.
             var rev_it = std.mem.reverseIterator(grid[shape_y - 1 - floor_y .. shape_y - 1 - floor_y + 4]);
             while (rev_it.next()) |row| {
                 if (!std.mem.allEqual(bool, &row, true)) continue;
@@ -70,27 +65,24 @@ pub fn main() !void {
                 std.mem.copy([7]bool, &grid, grid[new_floor_y - floor_y .. max_y - floor_y]);
                 std.mem.set([7]bool, grid[max_y - new_floor_y .. max_y - floor_y], .{false} ** 7);
                 floor_y = new_floor_y;
+                if (floor_y == max_y) { // Easy to detect a repeating floor with no blocks above it.
+                    if (repeat_start == null) {
+                        repeat_start = .{ .cmd_i = cmd_i, .shape_i = shape_i, .max_y = max_y };
+                    } else if (shape_i >= 2022 and repeat_start.?.cmd_i == cmd_i) { // Part of grid is repeating now; skip ahead.
+                        const shape_count = shape_i - repeat_start.?.shape_i;
+                        const mul = (1_000_000_000_000 - 1 - shape_i) / shape_count;
+                        shape_i += shape_count * mul;
+                        max_y += (max_y - repeat_start.?.max_y) * mul;
+                        floor_y = max_y;
+                    }
+                }
                 break;
             }
             shape_i += 1;
             shape_x = 2;
             shape_y = max_y + 4;
-            if (shape_i == 2022) {
-                p1 = max_y;
-                break; // TODO: Part 2
-            }
+            if (shape_i == 2022) try stdout.print("Day17 (comptime parsing): P1: {}", .{max_y});
         } else shape_y -= 1;
     }
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("Day17 (comptime parsing): P1: {}, P2: {}\n", .{ p1, 0 });
-}
-
-fn debugPrintGrid(grid: []const [7]bool, floor_y: usize, max_y: usize) void {
-    var rev_it = std.mem.reverseIterator(grid[0 .. max_y - floor_y]);
-    while (rev_it.next()) |row| {
-        std.debug.print("|", .{});
-        for (row) |solid| std.debug.print("{s}", .{if (solid) "#" else "."});
-        std.debug.print("|\n", .{});
-    }
-    std.debug.print("+-------+ <- y={}\n", .{floor_y});
+    try stdout.print(", P2: {}\n", .{max_y});
 }
