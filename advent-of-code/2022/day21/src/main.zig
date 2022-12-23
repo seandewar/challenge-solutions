191ab81f28ca644ec32df1fa269f7838ebb32463
Sean Dewar <seandewar@users.noreply.github.com>
2022-12-23 14:10:40 +0000

    AoC 2022: D21 P2
    Bisect and cry over miscompilations for great justice!

--- advent-of-code/2022/day21/src/main.zig
+++ advent-of-code/2022/day21/src/main.zig
@@ -17,11 +17,11 @@ const parsed_input = blk: {
     line_it.reset();
     for (monkeys) |*monkey| {
         const line = line_it.next().?;
-        if (std.ascii.isDigit(line[6])) {
-            monkey.* = .{ .name = line[0..4], .number = std.fmt.parseInt(i64, line[6..], 10) catch unreachable };
-        } else {
-            monkey.* = .{ .name = line[0..4], .unparsed_op_line = line }; // Resolve the operation and indices later.
-        }
+        monkey.* = .{ .name = line[0..4] };
+        if (std.ascii.isDigit(line[6]))
+            monkey.number = std.fmt.parseInt(i64, line[6..], 10) catch unreachable
+        else
+            monkey.unparsed_op_line = line; // Resolve the operation and indices later.
     }
     std.sort.sort(Monkey, &monkeys, {}, struct {
         fn lessThan(context: void, lhs: Monkey, rhs: Monkey) bool {
@@ -30,8 +30,10 @@ const parsed_input = blk: {
         }
     }.lessThan);
     var root_i: ?usize = null;
+    var humn_i: ?usize = null;
     for (monkeys) |*monkey, i| {
         if (root_i == null and std.mem.eql(u8, monkey.name, "root")) root_i = i;
+        if (humn_i == null and std.mem.eql(u8, monkey.name, "humn")) humn_i = i;
         if (monkey.number != null) continue;
         const line = monkey.unparsed_op_line;
         const compareFn = struct {
@@ -44,25 +46,60 @@ const parsed_input = blk: {
         monkey.rhs_i = std.sort.binarySearch(Monkey, .{ .name = line[13..] }, &monkeys, {}, compareFn).?;
         monkey.op = line[11];
     }
-    break :blk .{ .monkeys = monkeys, .root_i = root_i.? };
+    break :blk .{ .monkeys = monkeys, .root_i = root_i.?, .humn_i = humn_i.? };
 };
 
-fn computeAnswer(memo: *[parsed_input.monkeys.len]?i64, i: usize) i64 {
-    if (parsed_input.monkeys[i].number) |number| return number;
+fn computeAnswer(memo: *[parsed_input.monkeys.len]?i64, i: usize) ?i64 {
     if (memo[i]) |number| return number;
-    const lhs = computeAnswer(memo, parsed_input.monkeys[i].lhs_i);
-    const rhs = computeAnswer(memo, parsed_input.monkeys[i].rhs_i);
+    if (parsed_input.monkeys[i].number) |number| {
+        memo[i] = number;
+        return number;
+    }
+    const lhs = computeAnswer(memo, parsed_input.monkeys[i].lhs_i) orelse return null;
+    const rhs = computeAnswer(memo, parsed_input.monkeys[i].rhs_i) orelse return null;
     memo[i] = switch (parsed_input.monkeys[i].op) {
         '+' => lhs + rhs,
         '-' => lhs - rhs,
         '*' => lhs * rhs,
-        '/' => @divExact(lhs, rhs),
+        '/' => std.math.divExact(i64, lhs, rhs) catch return null,
         else => unreachable,
     };
     return memo[i].?;
 }
+fn checkP2Diff(memo: *[parsed_input.monkeys.len]?i64, number: *i64, retry_step: i64) i64 {
+    std.mem.set(?i64, memo, null);
+    memo[parsed_input.humn_i] = number.*;
+    _ = computeAnswer(memo, parsed_input.root_i) orelse {
+        number.* += retry_step;
+        return @call(.always_tail, checkP2Diff, .{ memo, number, retry_step });
+    };
+    return memo[parsed_input.monkeys[parsed_input.root_i].lhs_i].? - memo[parsed_input.monkeys[parsed_input.root_i].rhs_i].?;
+}
 pub fn main() !void {
     var memo = [_]?i64{null} ** parsed_input.monkeys.len;
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("Day21 (comptime parsing): P1: {}, P2: {}\n", .{ computeAnswer(&memo, parsed_input.root_i), 0 });
+    try stdout.print("Day21 (comptime parsing): P1: {}", .{computeAnswer(&memo, parsed_input.root_i).?});
+    blk: { // Why solve when you can waste time bisecting instead? :P
+        var l: i64 = 0;
+        var l_diff = checkP2Diff(&memo, &l, 1);
+        if (l_diff == 0) break :blk;
+        var r: i64 = std.math.maxInt(i53);
+        var r_diff = checkP2Diff(&memo, &r, -1);
+        if (r_diff == 0) break :blk;
+        while (l < r) {
+            var m = l + @divTrunc(r - l, 2);
+            const m_diff = checkP2Diff(&memo, &m, if (std.math.absCast(l_diff) < std.math.absCast(r_diff)) -1 else 1);
+            if (m_diff == 0) break :blk;
+            if ((l_diff < 0) != (m_diff < 0)) {
+                r = m - 1;
+                r_diff = checkP2Diff(&memo, &r, -1);
+                if (r_diff == 0) break :blk;
+            } else {
+                l = m + 1;
+                l_diff = checkP2Diff(&memo, &l, 1);
+                if (l_diff == 0) break :blk;
+            }
+        }
+    }
+    try stdout.print(", P2: {}\n", .{memo[parsed_input.humn_i].?});
 }
