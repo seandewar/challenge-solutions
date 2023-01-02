9a121b2a62e26e47d6265b4ec1d93c32125cd660
Sean Dewar <seandewar@users.noreply.github.com>
2023-01-02 07:59:40 +0000

    AoC 2022: D18: Simplify using SIMD @Vectors
    The SIMD is cool, but the good thing is that operators work element-wise on
    them.

--- advent-of-code/2022/day18/src/main.zig
+++ advent-of-code/2022/day18/src/main.zig
@@ -1,68 +1,53 @@
 const std = @import("std");
-
-const Vector = struct { x: i32 = 0, y: i32 = 0, z: i32 = 0 };
-fn gridIndexInBounds(i: Vector, size: Vector) bool {
-    return i.x >= 0 and i.y >= 0 and i.z >= 0 and i.x < size.x and i.y < size.y and i.z < size.z;
+fn gridIndexInBounds(i: @Vector(3, i32), size: @Vector(3, i32)) bool {
+    return i[0] >= 0 and i[1] >= 0 and i[2] >= 0 and i[0] < size[0] and i[1] < size[1] and i[2] < size[2];
 }
-fn parseNextCube(line: []const u8) Vector {
+fn parseNextCube(line: []const u8) @Vector(3, i32) {
     var it = std.mem.split(u8, line, ",");
     return .{
-        .x = std.fmt.parseInt(i32, it.next().?, 10) catch unreachable,
-        .y = std.fmt.parseInt(i32, it.next().?, 10) catch unreachable,
-        .z = std.fmt.parseInt(i32, it.next().?, 10) catch unreachable,
+        std.fmt.parseInt(i32, it.next().?, 10) catch unreachable,
+        std.fmt.parseInt(i32, it.next().?, 10) catch unreachable,
+        std.fmt.parseInt(i32, it.next().?, 10) catch unreachable,
     };
 }
+const dirs = [_]@Vector(3, i32){ .{ -1, 0, 0 }, .{ 1, 0, 0 }, .{ 0, -1, 0 }, .{ 0, 1, 0 }, .{ 0, 0, -1 }, .{ 0, 0, 1 } };
 const parsed_input = blk: {
     @setEvalBranchQuota(1_000_000);
     const input = @embedFile("input");
-    var bounds = [_]Vector{.{}} ** 2;
+    var bounds = [_]@Vector(3, i32){@splat(3, @as(i32, 0))} ** 2;
     var pos_count = 0;
     var line_it = std.mem.tokenize(u8, input, std.cstr.line_sep);
-    while (line_it.next()) |line| {
+    while (line_it.next()) |line| : (pos_count += 1) {
         const pos = parseNextCube(line);
-        for (@typeInfo(Vector).Struct.fields) |field| {
-            @field(bounds[0], field.name) = @min(@field(bounds[0], field.name), @field(pos, field.name));
-            @field(bounds[1], field.name) = @max(@field(bounds[1], field.name), @field(pos, field.name));
-        }
-        pos_count += 1;
+        bounds[0] = @min(bounds[0], pos);
+        bounds[1] = @max(bounds[1], pos);
     }
-    const size = Vector{
-        .x = bounds[1].x - bounds[0].x + 1,
-        .y = bounds[1].y - bounds[0].y + 1,
-        .z = bounds[1].z - bounds[0].z + 1,
-    };
-    var grid = std.mem.zeroes([size.z][size.y][size.x]usize);
-    var positions: [pos_count]Vector = undefined;
+    const size = bounds[1] - bounds[0] + @splat(3, @as(i32, 1));
+    var grid = std.mem.zeroes([size[2]][size[1]][size[0]]usize);
+    var positions: [pos_count]@Vector(3, i32) = undefined;
     line_it.reset();
     for (positions) |*pos| {
         pos.* = parseNextCube(line_it.next().?);
-        grid[pos.z - bounds[0].z][pos.y - bounds[0].y][pos.x - bounds[0].x] = 1;
+        const i = pos.* - bounds[0];
+        grid[i[2]][i[1]][i[0]] = 1;
     }
     var p1 = 6 * positions.len;
     for (positions) |pos| {
-        for ([_]Vector{ .{ .x = -1 }, .{ .x = 1 }, .{ .y = -1 }, .{ .y = 1 }, .{ .z = -1 }, .{ .z = 1 } }) |dir| {
-            const i = Vector{
-                .x = pos.x + dir.x - bounds[0].x,
-                .y = pos.y + dir.y - bounds[0].y,
-                .z = pos.z + dir.z - bounds[0].z,
-            };
-            if (gridIndexInBounds(i, size) and grid[i.z][i.y][i.x] == 1) p1 -= 1;
+        for (dirs) |dir| {
+            const i = pos + dir - bounds[0];
+            if (gridIndexInBounds(i, size) and grid[i[2]][i[1]][i[0]] == 1) p1 -= 1;
         }
     }
     break :blk .{ .p1 = p1, .grid = grid, .size = size, .bounds = bounds, .positions = positions };
 };
 
-fn floodCavity(grid: *@TypeOf(parsed_input.grid), i: Vector, value: usize) ?u32 {
-    grid[@intCast(usize, i.z)][@intCast(usize, i.y)][@intCast(usize, i.x)] = value;
+fn floodCavity(grid: *@TypeOf(parsed_input.grid), i: @Vector(3, i32), value: usize) ?u32 {
+    grid[@intCast(usize, i[2])][@intCast(usize, i[1])][@intCast(usize, i[0])] = value;
     var surface_area: u32 = 0;
-    for ([_]Vector{ .{ .x = -1 }, .{ .x = 1 }, .{ .y = -1 }, .{ .y = 1 }, .{ .z = -1 }, .{ .z = 1 } }) |dir| {
-        const j = Vector{
-            .x = i.x + dir.x,
-            .y = i.y + dir.y,
-            .z = i.z + dir.z,
-        };
+    for (dirs) |dir| {
+        const j = i + dir;
         if (!gridIndexInBounds(j, parsed_input.size)) return null;
-        const j_value = grid[@intCast(usize, j.z)][@intCast(usize, j.y)][@intCast(usize, j.x)];
+        const j_value = grid[@intCast(usize, j[2])][@intCast(usize, j[1])][@intCast(usize, j[0])];
         if (j_value == 1) surface_area += 1;
         if (j_value == value or j_value == 1) continue;
         if (j_value != 0) return null;
@@ -74,14 +59,10 @@ pub fn main() !void {
     var p2: u32 = parsed_input.p1;
     var grid = parsed_input.grid;
     for (parsed_input.positions) |pos, pos_i| {
-        for ([_]Vector{ .{ .x = -1 }, .{ .x = 1 }, .{ .y = -1 }, .{ .y = 1 }, .{ .z = -1 }, .{ .z = 1 } }) |dir, dir_i| {
-            const i = Vector{
-                .x = pos.x + dir.x - parsed_input.bounds[0].x,
-                .y = pos.y + dir.y - parsed_input.bounds[0].y,
-                .z = pos.z + dir.z - parsed_input.bounds[0].z,
-            };
+        for (dirs) |dir, dir_i| {
+            const i = pos + dir - parsed_input.bounds[0];
             if (!gridIndexInBounds(i, parsed_input.size)) continue;
-            if (grid[@intCast(usize, i.z)][@intCast(usize, i.y)][@intCast(usize, i.x)] != 0) continue;
+            if (grid[@intCast(usize, i[2])][@intCast(usize, i[1])][@intCast(usize, i[0])] != 0) continue;
             if (floodCavity(&grid, i, 2 + 6 * pos_i + dir_i)) |surface_area| p2 -= surface_area;
         }
     }
