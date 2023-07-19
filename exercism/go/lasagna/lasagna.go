f774f54628269fe594f3c4abae87a9495c1321cf
Sean Dewar <seandewar@users.noreply.github.com>
2023-07-19 21:23:43 +0100

    Exercism Go

---
+++ exercism/go/lasagna/lasagna.go
@@ -0,0 +1,15 @@
+package lasagna
+
+const OvenTime = 40
+
+func RemainingOvenTime(actualMinutesInOven int) int {
+	return OvenTime - actualMinutesInOven
+}
+
+func PreparationTime(numberOfLayers int) int {
+	return numberOfLayers * 2
+}
+
+func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
+	return PreparationTime(numberOfLayers) + actualMinutesInOven
+}
