ac6119427aef61d63f28c443c1cedaae8a38cf28
Sean Dewar <seandewar@users.noreply.github.com>
2023-08-04 23:07:12 +0100

    Exercism More Zig

---
+++ exercism/zig/raindrops/raindrops.zig
@@ -0,0 +1,8 @@
+pub fn convert(buffer: []u8, n: u32) []const u8 {
+    var stream = @import("std").io.fixedBufferStream(buffer);
+    if (n % 3 == 0) stream.writer().writeAll("Pling") catch unreachable;
+    if (n % 5 == 0) stream.writer().writeAll("Plang") catch unreachable;
+    if (n % 7 == 0) stream.writer().writeAll("Plong") catch unreachable;
+    if (stream.pos == 0) stream.writer().print("{}", .{n}) catch unreachable;
+    return stream.getWritten();
+}
