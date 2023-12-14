8feac0c330fee107dea8a10ea22ad571a0d449cb
Sean Dewar <seandewar@users.noreply.github.com>
2023-12-14 13:26:24 +0000

    AoC 2023 D14: rename tilt function, move gi closer to usage

--- advent-of-code/2023/day14/day14.cpp
+++ advent-of-code/2023/day14/day14.cpp
@@ -9,7 +9,6 @@ int main() {
     assert(f.is_open());
     std::string grid;
     int w = 0, h = 0;
-    const auto gi = [&](const int x, const int y) { return y * w + x; };
     for (std::string line; std::getline(f, line); ++h) {
         if (w == 0)
             w = line.size();
@@ -18,7 +17,8 @@ int main() {
     }
     assert(f.eof());
 
-    const auto tilt_line = [&](const int x, const int y, const bool horiz,
+    const auto gi = [=](const int x, const int y) { return y * w + x; };
+    const auto tilt_cell = [&](const int x, const int y, const bool horiz,
                                const int delta, int &dst) {
         char &c = grid[gi(x, y)];
         if (c != 'O') {
@@ -36,15 +36,15 @@ int main() {
     for (int i = 0;; ++i) {
         for (int x = 0; x < w; ++x)
             for (int y = 0, y_dst = 0; y < h; ++y)
-                tilt_line(x, y, false, 1, y_dst);
+                tilt_cell(x, y, false, 1, y_dst);
         for (int y = 0; y < h; ++y)
             for (int x = 0, x_dst = 0; x < w; ++x)
-                tilt_line(x, y, true, 1, x_dst);
+                tilt_cell(x, y, true, 1, x_dst);
 
         int load_after_i = 0, load_after_n_or_w = 0;
         for (int x = 0; x < w; ++x) {
             for (int y = h - 1, y_dst = h - 1; y >= 0; --y) {
-                if (tilt_line(x, y, false, -1, y_dst)) {
+                if (tilt_cell(x, y, false, -1, y_dst)) {
                     load_after_n_or_w += h - y;
                     load_after_i += h - y_dst - 1;
                 }
@@ -65,6 +65,6 @@ int main() {
 
         for (int y = 0; y < h; ++y)
             for (int x = w - 1, x_dst = w - 1; x >= 0; --x)
-                tilt_line(x, y, true, -1, x_dst);
+                tilt_cell(x, y, true, -1, x_dst);
     }
 }
