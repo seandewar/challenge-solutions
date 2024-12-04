ec6351cbf20b40ea10e41d355b74d475642876b3
Sean Dewar <6256228+seandewar@users.noreply.github.com>
2024-12-04 09:43:43 +0000

    Add Make String A Subsequence Using Cyclic Increments Solution

---
+++ leetcode/medium/make-string-a-subsequence-using-cyclic-increments.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/make-string-a-subsequence-using-cyclic-increments
+// Complexity: runtime O(n+m), space O(1).
+
+class Solution {
+public:
+    bool canMakeSubsequence(const string &str1, const string &str2) const
+    {
+        for (size_t i = 0, j = 0;; ++i) {
+            if (j == str2.size())
+                return true;
+            if (i + (str2.size() - j - 1) >= str1.size())
+                return false;
+
+            const char a = str1[i], b = str2[j];
+            if (a == b || (a + 1 - 'a') % ('z' + 1 - 'a') + 'a' == b)
+                ++j;
+        }
+    }
+};
