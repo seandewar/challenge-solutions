d5fdc01832c5842d08a6a0b178a04793c7829271
Sean Dewar <6256228+seandewar@users.noreply.github.com>
2026-01-12 23:21:58 +0000

    AoC 25 D2

--- advent-of-code/2025/day2/day2.cpp
+++ advent-of-code/2025/day2/day2.cpp
@@ -1,7 +1,9 @@
 #include <algorithm>
+#include <charconv>
 #include <fstream>
 #include <print>
 #include <ranges>
+#include <span>
 #include <string>
 #include <string_view>
 
@@ -16,8 +18,9 @@ int main()
     }
 
     std::uint64_t p1 = 0;
-    std::string num_buf;
+    std::uint64_t p2 = 0;
 
+    std::string num_buf;
     for (std::size_t start = 0; start < line.size();) {
         std::size_t end = line.find(',', start);
         if (end == std::string::npos)
@@ -27,94 +30,70 @@ int main()
         const std::size_t dash_pos = entry.find('-');
         if (dash_pos == std::string::npos)
             std::abort();
-        const std::string_view a_str = entry.substr(0, dash_pos);
-        const std::string_view b_str = entry.substr(dash_pos + 1);
+        const std::string_view a = entry.substr(0, dash_pos);
+        const std::string_view b = entry.substr(dash_pos + 1);
 
-        const auto non_digit = [](const char c) { return c < '0' || c > '9'; };
-        if (std::ranges::any_of(a_str, non_digit)
-            || std::ranges::any_of(b_str, non_digit)
-            || (a_str.size() > 1 && a_str[0] == '0')
-            || (b_str.size() > 1 && b_str[0] == '0'))
-            std::abort();
+        std::uint64_t a_int = 0;
+        std::uint64_t b_int = 0;
+        {
+            const auto [pa, aec] =
+                std::from_chars(a.data(), a.data() + a.size(), a_int);
+            if (aec != std::errc() || pa != a.data() + a.size())
+                std::abort();
 
-        // Prepare num_buf to hold half of the digits of each checked number.
-        // num_pos points to the most significant digit in num_buf.
+            const auto [pb, bec] =
+                std::from_chars(b.data(), b.data() + b.size(), b_int);
+            if (bec != std::errc() || pb != b.data() + b.size())
+                std::abort();
+        }
+
         num_buf.clear();
-        num_buf.resize((b_str.size() + 1) / 2, '0');
+        num_buf.resize(b.size(), '0');
+        std::size_t num_start = num_buf.size() - a.size();
 
-        std::size_t num_pos = 0;
-        const auto incr_num_buf = [&] {
-            for (char &c : num_buf | std::views::reverse) {
-                if (c++ < '9')
-                    return;
-                c = '0';
+        const auto num_span = [&] -> std::span<char> {
+            return {num_buf.begin() + num_start, num_buf.end()};
+        };
+        std::ranges::copy(a, num_span().begin());
+
+        for (std::uint64_t num_int = a_int; num_int <= b_int; ++num_int) {
+            const std::span<char> num_sp = num_span();
+
+            for (std::size_t div = 2; div <= num_sp.size(); ++div) {
+                if (num_sp.size() % div != 0)
+                    continue;
+
+                const std::span<char> l =
+                    num_sp.subspan(0, num_sp.size() / div);
+
+                std::size_t i = l.size();
+                for (; i < num_sp.size(); i += l.size()) {
+                    const std::span<char> r = num_sp.subspan(i, l.size());
+                    if (!std::ranges::equal(l, r))
+                        break;
+                }
+
+                if (i >= num_sp.size()) {
+                    if (div == 2)
+                        p1 += num_int;
+                    p2 += num_int;
+                    break;
+                }
             }
-        };
-        const auto num_view = [&] -> std::string_view {
-            return {num_buf.begin() + num_pos, num_buf.end()};
-        };
-        const auto buf_to_num = [&] -> std::uint64_t {
-            std::uint64_t num = 0;
-            for (const char c :
-                 std::views::repeat(num_view(), 2) | std::views::join)
-                num = num * 10 + (c - '0');
-            return num;
-        };
-        const auto num_gt_b = [&] -> bool {
-            const std::string_view view = num_view();
-            const std::string_view b_left = b_str.substr(0, view.size());
-            const std::string_view b_right = b_str.substr(view.size());
 
-            return (
-                view.size() * 2 > b_str.size()
-                || (view.size() * 2 == b_str.size()
-                    && (view > b_left || (view == b_left && view > b_right))));
-        };
-
-        // Start num_buf with the most significant of a_str's digits. If a_str
-        // can't be split evenly, start as the lowest "half" number with one
-        // more digit. If duplicating num_buf would make a smaller number than
-        // a_str, increment num_buf.
-        num_pos = num_buf.size() - (a_str.size() / 2);
-        if (a_str.size() % 2 != 0) {
-            num_buf[--num_pos] = '1';
-        } else {
-            std::copy_n(a_str.begin(), a_str.size() / 2,
-                        num_buf.begin() + num_pos);
-            const std::string_view view = num_view();
-            if (view < a_str.substr(view.size()))
-                incr_num_buf();
-        }
-
-        std::println("{}-{}", a_str, b_str);
-        if (num_gt_b()) {
-            start = end + 1;
-            continue;
-        }
-
-        while (true) {
-            p1 += buf_to_num();
-            std::string_view view = num_view();
-            std::println("  {}{}", view, view);
-
-            if (std::ranges::all_of(view,
-                                    [](const char c) { return c == '9'; })) {
-                if (num_pos == 0)
+            for (std::size_t i = num_buf.size(); i-- > 0;) {
+                char &c = num_buf[i];
+                if (c++ < '9')
                     break;
 
-                std::ranges::fill(num_buf, '0');
-                num_buf[--num_pos] = '1';
-                view = num_view();
-            } else {
-                incr_num_buf();
+                c = '0';
+                if (i == num_start)
+                    --num_start;
             }
-
-            if (num_gt_b())
-                break;
         }
 
         start = end + 1;
     }
 
-    std::println("Day2: P1: {}", p1);
+    std::println("Day2: P1: {}, P2: {}", p1, p2);
 }
